73e15e4788e3109641ee49725beb8808795f8f5b
Akshatha MK <92522733+Akshathamk-123@users.noreply.github.com>
2022-08-14 02:19:09 +0530

    Adding routes

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const ejs = require("ejs");
 const _ = require("lodash");  // for LODASH
 
 var homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-var aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-var contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+var aboutContent = "About us";
+var contactContent = "";
+var investorsContent ="Funding refers to the money required to start and run a business. It is a financial investment in a company for product development, manufacturing, expansion, sales and marketing, office spaces, and inventory.";
+var resourcesContent ="";
+var tipsContent ="";
+var stratergiesContent="";
+var similarContent="";
+
 
 const posts = [];
 const app = express();
@@ -32,19 +38,19 @@ app.get("/contact",function(req,res){
   res.render("contact",{key_3: contactContent});
 })
 
-app.get("/compose",function(req,res){
-  res.render("compose");
-})
-app.post("/compose",function(req,res){
-
-  var text = {
-    text_title: req.body.title,
-    text_post: req.body.post
-  };
-
-  posts.push(text);
-  res.redirect("/");
-})
+// app.get("/compose",function(req,res){
+//   res.render("compose");
+// })
+// app.post("/compose",function(req,res){
+//
+//   var text = {
+//     text_title: req.body.title,
+//     text_post: req.body.post
+//   };
+//
+//   posts.push(text);
+//   res.redirect("/");
+// })
 
 app.get("/posts/:topic",function(req,res){
   const topic = _.lowerCase(req.params.topic);
@@ -59,6 +65,27 @@ app.get("/posts/:topic",function(req,res){
 
 });
 })
+
+app.get("/investor",function(req,res){
+  res.render("investor",{key_4:investorsContent});
+})
+
+app.get("/resources",function(req,res){
+  res.render("resources",{key_5:resourcesContent});
+})
+
+app.get("/tips",function(req,res){
+  res.render("tips",{key_6:tipsContent});
+})
+
+app.get("/stratergies",function(req,res){
+  res.render("stratergies",{key_7:stratergiesContent});
+})
+
+app.get("/similar",function(req,res){
+  res.render("similar",{key_8:similarContent});
+})
+
 
 app.listen(2000, function() {
   console.log("Server started on port 2000");
